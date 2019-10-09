a44295761b2fd2678be1ab685db30c5b45eaa0ca
michalmelis <michalmelis.sk@gmail.com>
2019-10-09 17:13:52 +0200

    open-lab-01.2

---
+++ open-lab-01.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace open_lab_01._2
+{
+    class Program
+    {
+
+          public static int Sum(int firstnumber, int secondnumber)
+          {
+            return (firstnumber + secondnumber);
+          }
+        
+    }
+}
